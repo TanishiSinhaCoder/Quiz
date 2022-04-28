afaa12d68c69650b75c24318e3a794d71c8490e1
TanishiSinhaCoder <SinhaTanishi@outlook.com>
2022-04-28 14:20:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,108 @@
+var myQuestions = [
+	
+    {
+		question: "Where is the statue of liberty ?",
+		answers: {
+			a: 'Dubai',
+			b: 'New York',
+			c: 'America'
+		},
+		correctAnswer: 'b'
+	},
+ 
+    {
+		question: "What where the first biycycles called ?",
+		answers: {
+			a: 'hero cycles',
+			b: 'moving car',
+			c: 'boneshakers'
+		},
+		correctAnswer: 'c'
+	},
+
+    {
+		question: "what is the use of antena in a insects body?",
+		answers: {
+			a: 'feel and smell',
+			b: 'fly',
+			c: 'see and move'
+		},
+		correctAnswer: 'a'
+	},
+
+	
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+		
+		var output = [];
+		var answers;
+ 
+	
+		for(var i=0; i<questions.length; i++){
+			
+
+			answers = [];
+
+			for(letter in questions[i].answers){
+
+			
+				answers.push(
+					'<label>'
+						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+						+ letter + ': '
+						+ questions[i].answers[letter]
+					+ '</label>'
+				);
+			}
+
+			
+			output.push(
+				'<div class="question">' + questions[i].question + '</div>'
+				+ '<div class="answers">' + answers.join('') + '</div>'
+			);
+		}
+
+		quizContainer.innerHTML = output.join('');
+	}
+
+
+	function showResults(questions, quizContainer, resultsContainer){
+		
+		var answerContainers = quizContainer.querySelectorAll('.answers');
+		
+		var userAnswer = '';
+		var numCorrect = 0;
+		
+		for(var i=0; i<questions.length; i++){
+
+			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+			
+			if(userAnswer===questions[i].correctAnswer){
+				numCorrect++;
+				
+				answerContainers[i].style.color = 'lightgreen';
+			}
+			else{
+				answerContainers[i].style.color = 'red';
+			}
+		}
+
+		resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+	}
+
+	showQuestions(questions, quizContainer);
+	
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+
+}
